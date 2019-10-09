f44ec41aff2404457100b02eca8ef375da5dc827
inv0w <jonjoninfante@gmail.com>
2019-10-08 21:32:53 -0700

    Heroku Check

--- app.py
+++ app.py
@@ -28,4 +28,4 @@ def products_index():
     return render_template('products_index.html', products=products)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
