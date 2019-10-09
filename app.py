bd043b2c4fdb5c87ac74e1eb4c6019bdda51dc54
inv0w <jonjoninfante@gmail.com>
2019-10-08 19:41:59 -0700

    Products Page

--- app.py
+++ app.py
@@ -7,5 +7,18 @@ def index():
     """Return homepage."""
     return render_template('home.html', msg='Home Message')
 
+# OUR MOCK ARRAY OF Store Products
+products = [
+    { 'title': 'Dragon Kite', 'description': 'Wow dragon' },
+    { 'title': 'Bunny Kite', 'description': 'wow bunny' }
+]
+
+@app.route('/products')
+def products_index():
+    """Show all playlists."""
+    return render_template('products_index.html', products=products)
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
